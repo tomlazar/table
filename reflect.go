297b0171d77bb1816335d1769a5608cf20666f7e
Tom Lazar <thomas.lazar@marquette.edu>
2020-09-22 19:23:04 -0500

    add reflection parsers for structs and maps

--- reflect.go
+++ reflect.go
@@ -32,10 +32,54 @@ func MarshalTo(w io.Writer, arr interface{}, c *Config) error {
 // parse is the main method for refletion right now
 func parse(arr interface{}) (*Table, error) {
 	v := reflect.ValueOf(arr)
-	if v.Kind() != reflect.Slice {
-		return nil, errors.New("arr must be a slice")
+	switch v.Kind() {
+	case reflect.Slice:
+		return parseSlice(v)
+	case reflect.Struct:
+		return parseStruct(v)
+	case reflect.Map:
+		return parseMap(v)
 	}
 
+	return nil, errors.New("unknown interface type")
+}
+
+func parseMap(v reflect.Value) (*Table, error) {
+	tab := &Table{
+		Headers: []string{"Key", "Value"},
+		Rows:    make([][]string, len(v.MapKeys())),
+	}
+
+	iter := v.MapRange()
+	i := 0
+	for iter.Next() {
+		tab.Rows[i] = []string{fmt.Sprintf("%v", iter.Key().Interface()), fmt.Sprintf("%v", iter.Value().Interface())}
+
+		i++
+	}
+
+	return tab, nil
+}
+
+func parseStruct(v reflect.Value) (*Table, error) {
+	tab := &Table{
+		Headers: []string{"Field", "Type", "Value"},
+		Rows:    make([][]string, v.NumField()),
+	}
+	for i := 0; i < v.NumField(); i++ {
+		f := v.Type().Field(i)
+		n, ok := f.Tag.Lookup("table")
+		if !ok {
+			n = f.Name
+		}
+
+		tab.Rows[i] = []string{n, v.Field(i).Type().Name(), fmt.Sprintf("%v", v.Field(i).Interface())}
+	}
+
+	return tab, nil
+}
+
+func parseSlice(v reflect.Value) (*Table, error) {
 	if v.Len() < 1 {
 		return nil, errors.New("arr must have at least one element")
 	}
--- reflect_test.go
+++ reflect_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestMarshalTo(t *testing.T) {
+func TestMarshalToSlice(t *testing.T) {
 	data := []struct {
 		Name     string `table:"THE NAME"`
 		Location string `table:"THE LOCATION"`
@@ -21,4 +21,38 @@ func TestMarshalTo(t *testing.T) {
 	var buf bytes.Buffer
 	err := MarshalTo(&buf, data, nil)
 	require.NoError(t, err)
+
+	t.Log("\n" + buf.String())
+}
+
+type SomeStringer string
+
+func (s SomeStringer) String() string {
+	return "some string:" + string(s)
+}
+
+func TestMarshalStruct(t *testing.T) {
+	data := struct {
+		Name  string
+		Ty    string
+		Other SomeStringer
+	}{"the name of th", "tybal", SomeStringer("diso")}
+
+	buf, err := Marshal(data, nil)
+	require.NoError(t, err)
+
+	t.Log("\n" + string(buf))
+}
+
+func TestMarshalMap(t *testing.T) {
+	data := map[string]interface{}{
+		"Hello":    123,
+		"Test Key": "some other key",
+		"Nesting?": map[string]interface{}{"wow": "woo"},
+	}
+
+	buf, err := Marshal(data, nil)
+	require.NoError(t, err)
+
+	t.Log("\n" + string(buf))
 }
